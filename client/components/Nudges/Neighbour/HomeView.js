1e52aefd93d97716a937c34c9b6f2e5bd195f14b
gitexa <uafla@student.kit.edu>
2018-05-01 23:52:08 +0200

    Folder nudges and first draft neighbour nudge

---
+++ client/components/Nudges/Neighbour/HomeView.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { graphql, createFragmentContainer } from 'react-relay';
+import Page from 'components/Page/Page';
+
+import { withAuth } from 'modules/auth/utils';
+import { Button, Segment, Header, Label, Statistic, Form } from 'semantic-ui-react';
+import { Link } from 'found';
+import styles from './HomeView.scss';
+import classNames from 'classnames';
+
+
+
+class Neighbour extends React.Component {
+
+  render() {
+
+    return (
+    );
+  }
+}
+
+export default createFragmentContainer;
