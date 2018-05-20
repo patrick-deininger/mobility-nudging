ea381ed259c4e188e6b06c43000ac87426288f0f
Oruluk <pat.dein@gmail.com>
2018-05-20 03:14:24 +0200

    Embedded CreateSessionMutation in BeginningScreen

--- client/components/BeginningScreen/BeginningScreen.js
+++ client/components/BeginningScreen/BeginningScreen.js
@@ -6,17 +6,32 @@ import { withAuth } from 'modules/auth/utils';
 import { Button, Segment, Header, Label, Statistic, Form, Icon, Popup } from 'semantic-ui-react';
 import { Link } from 'found';
 import styles from './BeginningScreen.scss';
+import createSessionMutation from '../../modules/core/mutations/CreateSession';
 import classNames from 'classnames';
 
 
 class BeginningScreen extends React.Component {
 
-  // handleButtonClick = () => {
-  //   this.props.router.push('/')
-  // }
+  handleButtonClick = () => {
+    //this.props.router.push('/run/1')
+
+    const sessionVariables = {
+      user: this.props.viewer.user.id,
+      sessionConfig: this.props.viewer.sessionConfigs[0].id
+    };
+
+    console.log(sessionVariables)
+
+    createSessionMutation(this.props.relay.environment, sessionVariables, this.onCompletedCreateSession, this.setErrors)
+
+  }
+
+  onCompletedCreateSession = () => {
+    console.log("Done")
+  }
 
   render() {
-
+    console.log(this.props.viewer)
     return (
       <Page title='Mobility Nudging' viewer={this.props.viewer}>
         <section className={styles.container}>
@@ -24,7 +39,7 @@ class BeginningScreen extends React.Component {
             <p>Erläuterung:</p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             <p></p>
-            <Button as={Link} to='/run/1' fluid color="green" className={styles.conformationButton} >
+            <Button as={Link} to='/run/1' onClick={this.handleButtonClick} fluid color="green" className={styles.conformationButton} >
               Start
             </Button>
           </Segment>
@@ -47,7 +62,11 @@ export default createFragmentContainer(
           nudge
           chargeStatus
           nudge
-          clocktime
+
+      }
+      sessionConfigs{
+        id
+        name
       }
     }
   `,
